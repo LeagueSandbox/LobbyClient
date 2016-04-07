5b9200fefe238000da078c4812b3cf9fe04beca4
molenzwiebel <info@thijsmolendijk.nl>
2016-04-07 21:23:02 +0200

    Setup Angular

---
+++ src/ui/components/login/loginCtrl.js
@@ -0,0 +1,6 @@
+export default class LoginCtrl {
+	constructor($scope) {
+		$scope.message = "Hello, world!";
+	}
+}
+LoginCtrl.$inject = ["$scope"];
--- src/ui/main.js
+++ src/ui/main.js
@@ -1,1 +1,27 @@
-console.log(`Hello, world!`);
+"use strict";
+
+// Import the Babel polyfill, needed for generators.
+import "babel-polyfill";
+
+// Import Font Awesome.
+import "font-awesome/css/font-awesome.min.css";
+
+// Import Angular and dependencies.
+import "angular";
+import "angular-route";
+
+const app = angular.module("app", ["ngRoute"]);
+
+import loginCtrl from "./components/login/loginCtrl";
+import loginTemplate from "./components/login/loginView.html";
+
+// Setup controllers.
+app.controller("loginCtrl", loginCtrl);
+
+// Setup routes.
+app.config(["$routeProvider", prov => {
+	prov.when("/login", {
+		template: loginTemplate,
+		controller: "loginCtrl"
+	}).otherwise({ redirectTo: "/login" });
+}]);
